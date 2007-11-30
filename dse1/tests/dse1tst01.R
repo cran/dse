f33c91e2f3f123bb8d9c3aa0aff16c67b3aa92be
Paul Gilbert <pgilbert@bank-banque-canada.ca>
2007-11-30 00:00:00 +0000

    version 2007.11-1

--- dse1/tests/dse1tst01.R
+++ dse1/tests/dse1tst01.R
@@ -70,7 +70,8 @@ test.rng <- list(kind="Wichmann-Hill",seed=c(979,1479,1542),normal.kind="Box-Mul
 
   z <- residuals(VARmodelB)
   z <- acf(VARmodelB)
-# x11()
+  #x11()
+  get(getOption("device"))()
   acf(VARmodelB)
 
    good <- VARmodel$estimates$pred
